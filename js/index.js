89b315d4afe8a0ea8b4894257605f5a41399cb53
phucmp <phucmp@users.noreply.github.com>
2018-09-18 11:25:47 -0500

    Update Home Page
    For UI Design

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 // index.js 
 
 // Helper Functions
-function resize() {
-	document.getElementById("title").style.height = "50px";
-	console.log(document.getElementById("title").style);
-}
 
 
 // Main
