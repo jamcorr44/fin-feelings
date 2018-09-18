fa04acf9b0db4a9b0c692b1f4051a7d1bd001bb6
Jamie Corr <jamie.corr4@gmail.com>
2018-09-18 14:58:52 -0500

    js db

---
+++ js/db.js
@@ -0,0 +1,21 @@
+var person1 = {pid: 0, name: "Jamie Corr", location: "Los Angeles", description: "", image: "http://kb4images.com/images/picture-girl/38191181-picture-girl.jpg"};
+var person2 = {pid: 1, name: "Paul Pham", location: "Los Angeles", description: "", image: "images/p1.jpeg"};
+var person3 = {pid: 2, name: "Ava Biel", location: "San Francisco", description: "", image: "images/p2.jpeg"};
+var person4 = {pid: 3, name: "Emma Tseng", location: "Atlanta", description: "", image: "images/p3.jpeg"};
+var person5 = {pid: 4, name: "Ollie Auburn", location: "Seattle", description: "", image: "images/p4.jpeg"};
+var person6 = {pid: 5, name: "Max Wells", location: "Seattle", description: "", image: "images/p5.jpeg"};
+var person7 = {pid: 6, name: "Zoey Highland", location: "Boston", description: "", image: "images/p6.jpeg"};
+var person8 = {pid: 7, name: "KK Hernadez", location: "New York", description: "", image: "images/p7.jpeg"};
+
+var emotion1 = {id: 0, type: "inspired", color: "purple", rgb: "#764394"};
+var emotion2 = {id: 1, type: "sad", color: "blue", rgb: "#3277b5"};
+var emotion3 = {id: 2, type: "happy", color: "green", rgb: "#87b11d"};
+var emotion4 = {id: 3, type: "surprised", color: "yellow", rgb: "#ffcf14"};
+var emotion5 = {id: 4, type: "friendly", color: "orange", rgb: "#f16729"};
+var emotion6 = {id: 5, type: "angry", color: "red", rgb: "#ea2a29"};
+var emotion6 = {id: 6, type: "other", color: "grey", rgb: "#b0b2b7"};
+
+var people = [person1, person2, person3, person4, person5, person6, person7, person8];
+var emotions = [emotion1, emotion2, emotion3, emotion4, emotion5, emotion6];
+
+
