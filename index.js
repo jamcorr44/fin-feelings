82b90a6723bb98a5eece9e8800b7fbe4f036f203
phucmp <phucmp@users.noreply.github.com>
2018-09-18 10:45:14 -0500

    Update Home Page
    Added more css

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // index.js 
 
 // Helper Functions
-
+function resize() {
+	document.getElementById("title").style.height = "50px";
+	console.log(document.getElementById("title").style);
+}
 
 
 // Main
